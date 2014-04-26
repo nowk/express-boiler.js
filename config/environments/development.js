12f66826edb30f78485069717c1882780deb182d
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-04-25 22:29:59 -0400

    - Switch to Less - Development uses less middelware for compliation - Development adds static for assets/javascripts and css-cache paths

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,13 +1,43 @@
 /* jshint node: true */
 
+var path = require('path');
+var express = require('express');
 var morgan = require('morgan');
+var lessMiddleware = require('less-middleware');
 
 
 module.exports = function() {
   var app = this;
 
+  // assets-js and assets-css are for development, all js, css (less) will be compiled
+  // through `grunt build` during deployment
+
+  /*
+   * less middleware
+   */
+
+  var csscache = path.resolve(__dirname, '../../css-cache');
+  var less = lessMiddleware(path.resolve(__dirname, '../../app/assets'),
+    {
+      dest: csscache,
+      debug: true,
+      force: true
+    });
+
+
+  /*
+   * both these static references should load before static, this way these are hit before
+   * the commited css/js files that come from `grunt build`
+   */
+
+  var assetsjs = express.static(path.resolve(__dirname, '../../app/assets/javascripts'));
+  var assetscss = express.static(path.join(csscache, 'stylesheets'));
+
   app
     .middlewares()
-    .before('routes', {name: 'logger', cb: morgan({format: 'dev', immediate: true})});
+    .before('routes', {name: 'logger', cb: morgan({format: 'dev', immediate: true})})
+    .before('static', {name: 'assets-js', cb: assetsjs, path: '/javascripts'})
+    .after('assets-js', {name: 'less', cb: less})
+    .after('less', {name: 'assets-css', cb: assetscss, path: '/stylesheets'});
 };
 
--- config/environments/test.js
+++ config/environments/test.js
@@ -2,6 +2,21 @@
 
 
 module.exports = function() {
+  /*
+   * load development environment
+   */
+  var environment = require('./development');
+  environment.apply(this);
+
+
+  /*
+   * test configuration
+   */
+
   var app = this;
+
+  app
+    .middlewares()
+    .remove('logger');
 };
 
